6c99a70b50c2beae4b2a35bb486744684693859e
João Alves <joaomail@gmail.com>
2023-11-20 19:31:25 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-function Home(){
-    return <h1>Carla, eu amo você. Se você me ama, dê uma risadinha!!!</h1>
+function Home() {
+  return <h1>Carla, eu amo você. Se você me ama, dê uma risadinha!!!</h1>;
 }
 
+function Teste() {
+  return <h1>Teste</h1>;
+}
 export default Home;
